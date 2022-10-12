878e6aa62782ee729998a6d9d3d48017412b60cb
Mohamed Basem <eng_m.basem@outlook.com>
2022-10-12 17:39:09 +0200

    .

---
+++ DAL/Migrations/20221012153532_mkmsmds.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class mkmsmds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Ranking",
+                table: "PlayerGameWeaks",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$8yUWBK0OY9raQo.XlfSNMOPv9LZqG5rKSgPGjCTSPsrSdupwfFTyS");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ranking",
+                table: "PlayerGameWeaks");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$p6ldbBMB3Qfd7qpKsJuhc.kce02MjEyJRl1aK0wiVyhcYMOd.2/Mu");
+        }
+    }
+}

7ec0cc0a4002c9ce08e4e77d737905d47909ee0d
Mohamed Basem <eng_m.basem@outlook.com>
2022-12-17 14:12:13 +0200

    .

--- CoreServices/Logic/AccountTeamServices.cs
+++ CoreServices/Logic/AccountTeamServices.cs
@@ -307,13 +307,15 @@ namespace CoreServices.Logic
                                        .Team
                                        .HomeGameWeaks.Any(b => b.Fk_GameWeak == a.Fk_GameWeak &&
                                                                b.StartTime <= DateTime.UtcNow.AddHours(2) &&
-                                                               b.PlayerGameWeaks.Any(c => c.Fk_Player == a.AccountTeamPlayer.Fk_Player && c.PlayerGameWeakScores.Any())) ||
+                                                               b.PlayerGameWeaks.Any(c => c.Fk_Player == a.AccountTeamPlayer.Fk_Player &&
+                                                                                          (b.IsEnded == false || c.PlayerGameWeakScores.Any()))) ||
                                        a.AccountTeamPlayer
                                        .Player
                                        .Team
                                        .AwayGameWeaks.Any(b => b.Fk_GameWeak == a.Fk_GameWeak &&
                                                                b.StartTime <= DateTime.UtcNow.AddHours(2) &&
-                                                               b.PlayerGameWeaks.Any(c => c.Fk_Player == a.AccountTeamPlayer.Fk_Player && c.PlayerGameWeakScores.Any())),
+                                                               b.PlayerGameWeaks.Any(c => c.Fk_Player == a.AccountTeamPlayer.Fk_Player &&
+                                                                                          (b.IsEnded == false || c.PlayerGameWeakScores.Any()))),
                            Top15 = a.AccountTeamPlayer
                                     .Player
                                     .PlayerGameWeakScoreStates

027fe4756864e05821d3762c641aa06c5f1610c5
Abdelrahman Osama <eslamboully@gmail.com>
2023-06-22 17:29:46 +0300

    ...

---
+++ Entities/CoreServicesModels/PlayerMarkModels/MarkModel.cs
@@ -0,0 +1,32 @@
+﻿using Entities.RequestFeatures;
+
+namespace Entities.CoreServicesModels.PlayerMarkModels
+{
+    public class MarkParameters : RequestParameters
+    {
+        public List<int> Ids { get; set; }
+    }
+
+    public class MarkModel : AuditLookUpEntity
+    {
+        [DisplayName($"{nameof(Name)}")]
+        [Required(ErrorMessage = PropertyAttributeConstants.RequiredMsg)]
+        public new string Name { get; set; }
+    }
+
+    public class MarkCreateOrEditModel
+    {
+        [DisplayName($"{nameof(Name)}{PropertyAttributeConstants.ArLang}")]
+        [Required(ErrorMessage = PropertyAttributeConstants.RequiredMsg)]
+        public string Name { get; set; }
+
+        public MarkLangModel MarkLang { get; set; }
+    }
+
+    public class MarkLangModel
+    {
+        [DisplayName($"{nameof(Name)}{PropertyAttributeConstants.EnLang}")]
+        [Required(ErrorMessage = PropertyAttributeConstants.RequiredMsg)]
+        public string Name { get; set; }
+    }
+}

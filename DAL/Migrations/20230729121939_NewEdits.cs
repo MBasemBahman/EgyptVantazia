f1c6a763c04720dbc813e43912e1246bff51c4c5
Mohamed Basem <eng_m.basem@outlook.com>
2023-07-29 15:26:52 +0300

    .

---
+++ DAL/Migrations/20230729121939_NewEdits.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewEdits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$H1H02FtILnaOx//P2t9KHeK5lu/7.Y4XcR2dtjcjmb4C9lZDzesFm");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$Mhd90P4jwurLAkA1AVSUrOEVqOYLuAI4D41YaX.lGzKPjNMK4nR/C");
+        }
+    }
+}

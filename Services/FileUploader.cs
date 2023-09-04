eec236fd66f8550080f6af9df475ebaba2aff004
Reem Yasser <reem.yasser1210@gmail.com>
2023-09-04 17:22:25 +0300

    .

--- Services/FileUploader.cs
+++ Services/FileUploader.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.NetworkInformation;
 
 namespace Services
 {
@@ -24,6 +27,7 @@ namespace Services
             string fileName = file.FileName + DateTime.UtcNow.ToString("ddMMyyyyhhmmssfffffffK");
             fileName += Path.GetExtension(file.FileName);
 
+
             string folderName = Path.Combine(_rootPath, storagePath);
             if (!Directory.Exists(folderName))
             {
@@ -32,11 +36,29 @@ namespace Services
 
             string fullPath = Path.Combine(folderName, fileName);
 
-            using (FileStream localFile = File.OpenWrite(fullPath))
-            using (Stream uploadedFile = file.OpenReadStream())
+            if (file.ContentType.Contains("image"))
             {
-                await uploadedFile.CopyToAsync(localFile);
+                using (FileStream localFile = File.OpenWrite(fullPath))
+                using (Image uploadedFile = Image.FromStream(file.OpenReadStream()))
+                {
+                    var resized = new Bitmap(uploadedFile, new Size(600, 600));
+
+                    resized.SetResolution(70, 70);
+
+                    resized.Save(localFile, ImageFormat.Jpeg);
+                }
             }
+            else
+            {
+                using (FileStream localFile = File.OpenWrite(fullPath))
+                using (Stream uploadedFile = file.OpenReadStream())
+                {
+
+                    await uploadedFile.CopyToAsync(localFile);
+
+                }
+            }
+
             return storagePath + "/" + fileName;
         }
     }

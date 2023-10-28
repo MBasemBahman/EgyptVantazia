eb5c99b0218637f29af206869796749c0f5b3ea8
Mohamed Basem <eng_m.basem@outlook.com>
2023-10-28 17:49:00 +0200

    .

--- Dashboard/Utility/UpdateResultsUtils.cs
+++ Dashboard/Utility/UpdateResultsUtils.cs
@@ -11,7 +11,7 @@ namespace Dashboard.Utility
         public UpdateResultsUtils(ServicesHttpClient servicesHttp)
         {
             _servicesHttp = servicesHttp;
-            _servicesHttp.BaseUri = "https://egypt-fantasy-automate.azurewebsites.net/";
+            _servicesHttp.BaseUri = "https://automate.faleagues.com/";
         }
 
         public void UpdateStandings()
